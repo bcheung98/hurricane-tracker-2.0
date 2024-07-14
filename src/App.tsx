80e543a78def516c1256288be1b7555f5f994129
Brian <briantcheung@gmail.com>
2024-07-13 18:36:48 -0700

    Added theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,48 @@
+import { defaultTheme as theme } from "./theme"
+import { ThemeProvider } from "@mui/material/styles"
 import { Box, AppBar, CardHeader, Avatar, Typography } from "@mui/material"
 
 const App = () => {
 
 	return (
-		<Box
-			sx={{
-				height: "100vh",
-				backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://upload.wikimedia.org/wikipedia/commons/0/04/Hurricane_Isabel_from_ISS.jpg')",
-				backgroundPosition: "center",
-				backgroundRepeat: "no-repeat",
-				backgroundSize: "cover",
-				position: "relative"
-			}}
-		>
-			<AppBar position="static"
+		<ThemeProvider theme={theme}>
+			<Box
 				sx={{
-					borderBottom: "2px solid white"
+					height: "100vh",
+					backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://upload.wikimedia.org/wikipedia/commons/0/04/Hurricane_Isabel_from_ISS.jpg')",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					position: "relative"
 				}}
 			>
-				<CardHeader
-					avatar={
-						null
-					}
-					title={
-						<Typography variant="h5"
-							sx={{
-								mr: 2,
-								display: { xs: "none", md: "flex" },
-								letterSpacing: ".3rem",
-								color: "white",
-								fontWeight: "bold",
-							}}
-						>
-							Hurricane Tracker
-						</Typography>
-					}
-				/>
-			</AppBar>
-		</Box>
+				<AppBar position="static"
+					sx={{
+						backgroundColor: `${theme.appbar.backgroundColor}`,
+						borderBottom: `2px solid ${theme.border.color}`
+					}}
+				>
+					<CardHeader
+						avatar={
+							null
+						}
+						title={
+							<Typography variant="h5"
+								sx={{
+									mr: 2,
+									display: { xs: "none", md: "flex" },
+									letterSpacing: ".3rem",
+									color: "white",
+									fontWeight: "bold",
+								}}
+							>
+								Hurricane Tracker
+							</Typography>
+						}
+					/>
+				</AppBar>
+			</Box>
+		</ThemeProvider>
 	)
 
 }
