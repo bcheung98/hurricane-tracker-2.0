2f0a7ab0403661ad9fd180045b13fbfa5275d732
Brian <briantcheung@gmail.com>
2024-07-13 18:08:39 -0700

    Added basic landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-function App() {
+import { Box } from "@mui/material"
+
+const App = () => {
+
 	return (
-		<div>
-		</div>
+		<Box
+			sx={{
+				height: "100vh",
+				backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://upload.wikimedia.org/wikipedia/commons/0/04/Hurricane_Isabel_from_ISS.jpg')",
+				backgroundPosition: "center",
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				position: "relative"
+			}}
+		>
+		</Box>
 	)
+
 }
 
 export default App
